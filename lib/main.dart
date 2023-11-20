192a6a596f859c0e0af7788fefc6dcaeab2992d3
vhocdev <victor.infodev@gmail.com>
2023-11-20 03:20:58 -0300

    Tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -3,123 +3,221 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        scaffoldBackgroundColor:const Color.fromRGBO(169, 1, 247, 1),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 15,
+        toolbarHeight: 250,
+        backgroundColor: Colors.white,
+        flexibleSpace: const Image(
+          image: AssetImage('images/lovepeople_logo.png'),
+          fit: BoxFit.scaleDown,
+          height: 100
+        ),
+        title: const Text(
+          'lovepeople',
+          style: TextStyle(color: Color.fromRGBO(49, 1, 185, 1), fontSize: 16),
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(250),
+          )
+        ),
+        centerTitle: true
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(children: <Widget>[
+        Row(
+          mainAxisAlignment : MainAxisAlignment.center,
+          children: [         
+            Container(
+              margin: const EdgeInsets.only(top: 50.0, bottom: 40.0),
+              child: const Text(
+                'Que bom que voltou!',
+                style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1), fontSize: 28),                
+              ),          
             ),
           ],
         ),
+        const Row(
+          mainAxisAlignment : MainAxisAlignment.center,
+          children: [         
+            SizedBox(
+              width: 360,
+              child: TextField(
+                style: TextStyle(color: Color.fromRGBO(49, 1, 185, 1)),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(8),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(14))),
+                  hintText: 'Número de telefone, email ou CPF',
+                  hintStyle: TextStyle(color: Color.fromRGBO(49, 1, 185, 1)),
+                  filled: true,
+                  fillColor: Color.fromRGBO(255, 255, 255, 1),
+              )),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 16.0
+        ),        
+        const Row(
+          mainAxisAlignment : MainAxisAlignment.center,
+          children: [        
+            SizedBox(
+              width: 360,
+              child: TextField(
+                style: TextStyle(color: Color.fromRGBO(49, 1, 185, 1)),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(8),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(14))),
+                  hintText: 'Senha',
+                  hintStyle: TextStyle(color: Color.fromRGBO(49, 1, 185, 1)),
+                  filled: true,
+                  fillColor: Color.fromRGBO(255, 255, 255, 1),
+              )),
+            ),
+          ],
+        ), 
+        Row(
+          mainAxisAlignment : MainAxisAlignment.spaceAround,  
+          children: [        
+          Container(
+            margin: const EdgeInsets.only(top: 15.0, bottom: 30.0),
+            child: RichText(
+            text: TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              children: const <TextSpan>[
+                TextSpan(
+                  text: 'Esqueceu seu login ou senha?',
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 255, 255, 1), 
+                    decoration: TextDecoration.none, 
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),                
+                ),
+                TextSpan(
+                  text: ' Clique aqui',
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 214, 0, 1), 
+                    decoration: TextDecoration.none, 
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),                
+                ),
+              ],
+            ),
+            ),                  
+          )],
+        ),
+        Row(
+          mainAxisAlignment : MainAxisAlignment.spaceAround,  
+          children: [        
+           SizedBox(
+            width: 140,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () {
+              },
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                backgroundColor: const Color.fromRGBO(49, 1, 185, 1),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),
+                side: const BorderSide(color: Color.fromRGBO(255, 255, 255, 1), width: 2.0),
+              ),
+              ),
+              child: const Text('Entrar'),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      Row(children: [
+        Expanded(
+          child: Container(
+            width: 600,
+            margin: const EdgeInsets.only(top: 110.0, bottom: 30.0),
+            child: CustomPaint(
+              painter: DashedLinePainter(),
+              size: const Size(200.0, 1), // Tamanho da linha
+            ), 
+          )
+        )
+      ]),
+      Center(
+          child: RichText(
+            text: TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              children: const <TextSpan>[
+                TextSpan(
+                  text: 'Não possui cadastro?',
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 255, 255, 1), 
+                    decoration: TextDecoration.none, 
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),                
+                ),
+                TextSpan(
+                  text: ' Clique aqui',
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 214, 0, 1), 
+                    decoration: TextDecoration.none, 
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),                
+                ),
+              ],
+            ),
+          ),
+        )         
+      ]));
+  }
+}
+
+class DashedLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = const Color.fromRGBO(255, 255, 255, 1)
+      ..strokeWidth = 2.0
+      ..strokeCap = StrokeCap.round;
+
+    const double dashWidth = 5.0;
+    const double dashSpace = 5.0;
+
+    double startX = 0.0;
+
+    while (startX < size.width) {
+      canvas.drawLine(Offset(startX, size.height / 2),
+          Offset(startX + dashWidth, size.height / 2), paint);
+      startX += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
